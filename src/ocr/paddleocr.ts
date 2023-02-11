330a9436d6b62cbc1d64c0c1020f25e613fc1693
rerender2021 <rerender2021@outlook.com>
2023-02-11 16:16:50 +0800

    impl init

--- src/ocr/paddleocr.ts
+++ src/ocr/paddleocr.ts
@@ -1,5 +1,8 @@
 import { IOcrEngine, IOcrEngineOptions, IOcrResult } from "./base";
 import axios from "axios";
+import path from "path";
+import fs from "fs";
+import childProcess from "child_process";
 
 export interface IPaddleOcrResult extends IOcrResult {
 	detail: Array<IPaddleOcrItem>;
@@ -18,16 +21,63 @@ export type IPaddleOcrItem = [
 
 export class PaddleOcrEngine implements IOcrEngine {
 	private options: IOcrEngineOptions;
+	private ocr: childProcess.ChildProcessWithoutNullStreams;
 
 	constructor(options: IOcrEngineOptions) {
 		this.options = options;
 	}
 
-	async init() {}
+	async init() {
+		console.log("init called");
+		const paddleDir = path.resolve(process.cwd(), "paddle-server");
+		console.log("paddleDir: ", paddleDir);
+		if (fs.existsSync(paddleDir)) {
+			return new Promise((resolve, reject) => {
+				console.log("paddleDir exists, start ocr server");
 
-	async destroy() {}
+				/** caution: use npm run dev:once, or it will throw error "error while attempting to bind on address" */
+				const ocr = childProcess.spawn(`./paddle-server/PaddleocrAPI.exe`, [`--lang=ch`, `--model-dir=.\\model`]);
+				this.ocr = ocr;
+				ocr.stdout.on("data", (data) => {
+					console.log(`stdout: ${data}`);
+					if (data.includes("PaddleocrAPI has been started")) {
+						console.log("ocr server started");
+						resolve(true);
+					}
+				});
+
+				ocr.stderr.on("data", (data) => {
+					console.error(`stderr: ${data}`);
+				});
+
+				ocr.on("close", (code) => {
+					console.log(`ocr server exit: ${code}`);
+					reject(false);
+				});
+			});
+		}
+	}
+
+	async destroy() {
+		if (this.ocr) {
+			console.log("exit ocr server process");
+			this.ocr.kill(0);
+		}
+	}
 
 	async recognize(buffer: Buffer): Promise<IOcrResult> {
+		try {
+			const response = await axios.get("http://localhost:8000/ping", {});
+			console.log(`ping result`, response.data);
+
+			if (response.data !== "pong") {
+				return { text: "ocr server is not running" };
+			}
+		} catch (error) {
+			console.log(error);
+			return { text: "" };
+		}
+
 		const base64 = buffer.toString("base64");
 		console.time("ocr time");
 		let text = "";
@@ -45,7 +95,7 @@ export class PaddleOcrEngine implements IOcrEngine {
 				}
 			});
 		} catch (error) {
-			console.log(error);
+			console.log(error.message);
 			this.options.onError(error.message);
 		}
 		console.log(`ocr result:\n`);
